8bbf5fd48d603843aa65fd49db424ae4458ef382
ghulakis <55784122+ghulakis@users.noreply.github.com>
2024-12-10 15:58:37 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 st.title("Finalytica: Your Accounting Assistant")
 st.sidebar.header("Navigation")
-st.sidebar.select_slider("Choose a Feature", ["Dashboard", "Financial Analysis", "Tax Calculations"])
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+                        "",
+                         ("Choose a Feature", ["Dashboard", "Financial Analysis", "Tax Calculations"])
+                         )
 st.write("Welcome to Finalytica, your AI-powered accounting assistant!")
